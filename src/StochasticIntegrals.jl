45ef4c5b9c8c1668bf16bf86a4922a60008496f2
Stuart Baumann <stuart@stuartbaumann.com>
2019-01-24 19:45:53 +0000

    Changing name of files

--- src/StochasticIntegrals.jl
+++ src/StochasticIntegrals.jl
@@ -7,7 +7,7 @@ using Distributions: Normal, quantile
 using Sobol: SobolSeq, next!
 using Random
 
-include("Brown.jl")
+include("1_main_functions.jl")
 
 export ItoIntegral, get_volatility, get_variance, get_covariance, get_correlation, ItoSet, CovarianceAtDate
 export get_normal_draws, get_sobol_normal_draws, get_zero_draws, pdf, make_covariance, log_likelihood
