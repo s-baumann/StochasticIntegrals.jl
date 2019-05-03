e372cf9e0d72ac5eca40f10d58ffb6be1c5caf50
Stuart Baumann <stuart@stuartbaumann.com>
2019-05-03 22:54:30 +0100

    Removing Importance Sampling

--- src/4_number_generators.jl
+++ src/4_number_generators.jl
@@ -27,21 +27,21 @@ function next!(number_generator::Mersenne)
     return rand(number_generator.twister_, number_generator.number_of_itos_)
 end
 
-#"""
-#    ImportanceSampler(underlying_generator_::NumberGenerator, conversion_function_::Function)
-#    The importance sampler takes in a NumberGenerator that is used to get a random vector of uniforms,  [0,1]^d.
-#    Then it inputs this vector into a function (the conversion_function): [0,1]^d -> [0,1]^d.
-#    Note that if the identity function is input then the numbers generated are the same as the input
-#    number generator. If on the other hand a convex function (in some dimension) is input it is less likely to
-#    get a high number for that dimension.
-#    If you are doing Importance Sampling remember to multiply the results by the inverse of the pdf later. This
-#    package doesn't do that for you.
-#"""
-#struct ImportanceSampler <: NumberGenerator
-#    underlying_generator_::NumberGenerator
-#    conversion_function_::Function
-#end
-#function next!(number_generator::ImportanceSampler)
-#    array_of_uniforms = next!(number_generator.underlying_generator_)
-#    return number_generator.conversion_function_(array_of_uniforms)
-#end
+"""
+    ImportanceSampler(underlying_generator_::NumberGenerator, conversion_function_::Function)
+    The importance sampler takes in a NumberGenerator that is used to get a random vector of uniforms,  [0,1]^d.
+    Then it inputs this vector into a function (the conversion_function): [0,1]^d -> [0,1]^d.
+    Note that if the identity function is input then the numbers generated are the same as the input
+    number generator. If on the other hand a convex function (in some dimension) is input it is less likely to
+    get a high number for that dimension.
+    If you are doing Importance Sampling remember to multiply the results by the inverse of the pdf later. This
+    package doesn't do that for you.
+"""
+struct ImportanceSampler <: NumberGenerator
+    underlying_generator_::NumberGenerator
+    conversion_function_::Function
+end
+function next!(number_generator::ImportanceSampler)
+    array_of_uniforms = next!(number_generator.underlying_generator_)
+    return number_generator.conversion_function_(array_of_uniforms)
+end
