45f46e2d4580e36af8469d33ac1b8fdb53cd08c0
Stuart Baumann <stuart@stuartbaumann.com>
2020-10-10 18:54:10 +0100

    Update 6_ItoProcesses.jl

--- src/6_ItoProcesses.jl
+++ src/6_ItoProcesses.jl
@@ -71,28 +71,3 @@ function make_ito_process_non_syncronous_time_series(ito_processes::Dict{Symbol,
     end
     return d1
 end
-
-
-function make_ito_process_non_syncronous_time_series_wide(ito_processes::Dict{Symbol,ItoProcess{R}},
-                                                     covar::ForwardCovariance, update_rates::Union{OrderedDict{Symbol,D},Dict{Symbol,D}},
-                                                     total_number_of_ticks::Integer; timing_twister::MersenneTwister = MersenneTwister(1), ito_twister = MersenneTwister(2)) where R<:Real where D<:Distribution
-    update_rates = OrderedDict(update_rates)
-    assets = collect(keys(ito_processes))
-    d1 = DataFrame(Time = Array{R}([]))
-    for c in assets
-        d1[!,c] = Array{Union{Missing,R}}([])
-    end
-    at_time = covar.to_
-    number_generator = Mersenne(ito_twister, length(assets))
-    for i in 1:total_number_of_ticks
-      starts = vcat(rand.(Ref(timing_twister), values(update_rates), 1)...)
-      next_tick = at_time + minimum(starts)
-      ito_processes, covar = evolve_covar_and_ito_processes(ito_processes, covar, next_tick; recalculate_all = false, number_generator = number_generator)
-      # Moving everything up to the tick.
-      what_stock = collect(keys(update_rates))[findall(abs.(starts .- minimum(starts)) .< 1e-15)[1]]
-      d2 = Dict([:Time, what_stock, setdiff(assets, [what_stock])...] .=> [next_tick, ito_processes[what_stock].value, repeat([missing], length(assets)-1)...])
-      d1 = push!(d1,d2)
-      at_time = next_tick
-    end
-    return d1
-end
